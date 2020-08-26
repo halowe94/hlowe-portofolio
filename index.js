899a1bb1fc35d873409a9b15a15746c748b7e8f9
H Lowe <haleylowe0@gmail.com>
2020-08-25 17:55:19 -0700

    added js file. added photos to html.

---
+++ index.js
@@ -0,0 +1,8 @@
+//create function to store item in local storage
+$("#dayPlanner").click( function() {
+    window.open("halowe94.github.io/calendar/");
+});
+
+$("#journeyOn").click( function() {
+    window.open("halowe94.github.io/JourneyOn/");
+});
