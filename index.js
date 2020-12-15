a3d38d4cc91dc19bf4c90810b8ed5e174d65591d
halowe <haleylowe0@gmail.com>
2020-12-15 15:51:40 -0800

    updated portfolio page with more recent projects and changed css on navbar and footer

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 //create function to store item in local storage
-$("#dayPlanner").click( function() {
-    window.open("https://halowe94.github.io/calendar/");
+$("#dayPlanner").click(function () {
+  window.open("https://halowe94.github.io/calendar/");
 });
 
-$("#journeyOn").click( function() {
-    window.open("https://halowe94.github.io/JourneyOn/");
+$("#journeyOn").click(function () {
+  window.open("https://halowe94.github.io/JourneyOn/");
 });
 
-$("#codingQuiz").click( function() {
-    window.open("https://halowe94.github.io/coding_quiz/");
+$("#dinnerTable").click(function () {
+  window.open("https://stark-stream-40899.herokuapp.com/");
+});
+
+$("#fitnessTracker").click(function () {
+  window.open("https://workout-tracker-mongo123.herokuapp.com/");
+});
+
+$("#nodeCLI").click(function () {
+  window.open("https://github.com/halowe94/nodeCLI/");
+});
+
+$("#empDir").click(function () {
+  window.open("https://halowe94.github.io/employeeDirectory/");
 });
